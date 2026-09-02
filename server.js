a46402b8ba33db4cf0f3261d1caa4ddd99b55f05
willian388 <126960626+willian388@users.noreply.github.com>
2026-09-02 15:58:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { app, migrate, seedDefaults } = require('./app');
+const { app, migrate, seedDefaults } = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
