013f3e6461e114e7c79e289d05b78387b7988913
willian388 <126960626+willian388@users.noreply.github.com>
2026-09-02 15:54:00 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { app, migrate, seedDefaults } = require('./app.js');
+const { app, migrate, seedDefaults } = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
